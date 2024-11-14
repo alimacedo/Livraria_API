a7dd47d481ca36f64773a73af451fc8fd1f4cf42
alimacedo <alimacedoapolinario@gmail.com>
2024-11-14 00:58:54 -0300

    Iniciar implementação e conexão com Banco de Dados

---
+++ src/test/java/macedo/alisson/livraria_api/LivrariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package macedo.alisson.livraria_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
